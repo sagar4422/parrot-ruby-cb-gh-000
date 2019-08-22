4f8ab9f3a2d0e245748f68ad5bbafcde615a56b9
Sagar Gururaj Deshpande <sagargururaj.d@gmail.com>
2019-08-22 17:56:22 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(type="Squawk!")
-  puts type
+def parrot(name="Squawk!")
+  puts name.to_string
 end
 
 parrot()
